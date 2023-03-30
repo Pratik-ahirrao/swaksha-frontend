71fb96fb3f1be3344da7554bd57c3a9ed9ee020a
Pratik-ahirrao <pratikahirrao0@gmail.com>
2023-03-30 12:05:10 +0530

    updated dashboard

--- patient-app/src/Components/table/Table.jsx
+++ patient-app/src/Components/table/Table.jsx
@@ -12,53 +12,48 @@ const List = () => {
   const rows = [
     {
       id: 1143155,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 785,
-      method: "Cash on Delivery",
-      status: "Approved",
+      name: "John Smith",
+      date: "07-03-2003",
+      condition: "Cold flu",
+      severity: "mild",
+      category: "Encounter Diagnosis",
+      prescription: "Amoxicillin ",
     },
     {
       id: 2235235,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Michael Doe",
-      date: "1 March",
-      amount: 900,
-      method: "Online Payment",
-      status: "Pending",
+      name: "Michael Doe",
+      date: "07-03-2003",
+      condition: "AIDS",
+      severity: "severe",
+      category: "Chemo therapy",
+      prescription: "Trizivir",
     },
     {
       id: 2342353,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 35,
-      method: "Cash on Delivery",
-      status: "Pending",
+      name: "Will Smith",
+      date: "07-03-2003",
+      condition: "Diarrhoea",
+      severity: "severe",
+      category: "Encounter Diagnosis",
+      prescription: "Entriquinol",
     },
     {
       id: 2357741,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Jane Smith",
-      date: "1 March",
-      amount: 920,
-      method: "Online",
-      status: "Approved",
+      name: "Jane Smith",
+      date: "07-03-2003",
+      condition: "Cold flu and fever",
+      severity: "low",
+      category: "Diagnosis",
+      prescription: "Sinarest",
     },
     {
-      id: 2342355,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Harold Carol",
-      date: "1 March",
-      amount: 2000,
-      method: "Online",
-      status: "Pending",
+      id: 2342355,      
+      name: "Harold Carol",
+      date: "07-03-2003",
+      condition: "Severe Headache",
+      severity: "severe",
+      category: "Diagnosis",
+      prescription: "Dolo 650",
     },
   ];
   return (
@@ -68,13 +63,13 @@ const List = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell className="tableCell">Tracking ID</TableCell>
-            <TableCell className="tableCell">Product</TableCell>
-            <TableCell className="tableCell">Customer</TableCell>
-            <TableCell className="tableCell">Date</TableCell>
-            <TableCell className="tableCell">Amount</TableCell>
-            <TableCell className="tableCell">Payment Method</TableCell>
-            <TableCell className="tableCell">Status</TableCell>
+            <TableCell className="tableCell">Doctor SSID</TableCell>
+            <TableCell className="tableCell">Doctor Name</TableCell>
+            <TableCell className="tableCell">Diagnosis Date</TableCell>
+            <TableCell className="tableCell">Condition</TableCell>
+            <TableCell className="tableCell">Severity</TableCell>
+            <TableCell className="tableCell">Category</TableCell>
+            <TableCell className="tableCell">Prescription</TableCell>
 
           </TableRow>
         </TableHead>
@@ -82,19 +77,13 @@ const List = () => {
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">
-                <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
-                  {row.product}
-                </div>
-              </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
+              <TableCell className="tableCell">{row.name}</TableCell>
               <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
-              <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">
-                <span className={`status ${row.status}`}>{row.status}</span>
-              </TableCell>
+              <TableCell className="tableCell">{row.condition}</TableCell>
+              <TableCell className="tableCell">{row.severity}</TableCell>
+              <TableCell className="tableCell">{row.category}</TableCell>
+              <TableCell className="tableCell">{row.prescription}</TableCell>
+              
              
             </TableRow>
           ))}
