bd5e80181f614619f6f14f6029f0d4871a73daf8
Pratyush <ayushtiwaryy22@gmail.com>
2023-05-11 14:44:37 +0530

    i18

--- patient-app/src/Components/navbar/Navbar.jsx
+++ patient-app/src/Components/navbar/Navbar.jsx
@@ -9,54 +9,59 @@ import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlin
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
-const Navbar = () => {
-    // const { dispatch } = useContext(DarkModeContext);
-    const [notifications, setNotifications] = useState(localStorage.getItem("notifications"));
-    
-    useEffect(()=>{
-      setNotifications(localStorage.getItem("notifications"));
-      const handleStorage = () => {
-        setNotifications(localStorage.getItem("notifications"));
-      }
-      window.addEventListener('storage', handleStorage);
-      return () => window.removeEventListener("storage", handleStorage);
-    }, []);
+import { useTranslation } from "react-i18next";
+import LanguageSelect from "./LanguageSelect.jsx";
 
-    return (
-      <div className="navbar">
-          <div className="wrapper">
+const Navbar = () => {
+  // const { dispatch } = useContext(DarkModeContext);
+  const [notifications, setNotifications] = useState(localStorage.getItem("notifications"));
+  const { t } = useTranslation();
+  
+  useEffect(()=>{
+    setNotifications(localStorage.getItem("notifications"));
+    const handleStorage = () => {
+      setNotifications(localStorage.getItem("notifications"));
+    }
+    window.addEventListener('storage', handleStorage);
+    return () => window.removeEventListener("storage", handleStorage);
+  }, []);
+
+  return (
+    <div className="navbar">
+        <div className="wrapper">
+      
+      <div className="items">
+        <div className="item">
+          <LanguageOutlinedIcon className="icon" />
+          <LanguageSelect />
+          English
+        </div>
         
-        <div className="items">
-          <div className="item">
-            <LanguageOutlinedIcon className="icon" />
-            English
-          </div>
-         
-          <div className="item">
-            <FullscreenExitOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
-            <NotificationsNoneOutlinedIcon className="icon" />
-            <div className="counter">{notifications}</div>
-          </div>
-          <div className="item">
-            <ChatBubbleOutlineOutlinedIcon className="icon" />
-            <div className="counter">2</div>
-          </div>
-          <div className="item">
-            <ListOutlinedIcon className="icon" />
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-              className="avatar"
-            />
-          </div>
+        <div className="item">
+          <FullscreenExitOutlinedIcon className="icon" />
+        </div>
+        <div className="item">
+          <NotificationsNoneOutlinedIcon className="icon" />
+          <div className="counter">{notifications}</div>
+        </div>
+        <div className="item">
+          <ChatBubbleOutlineOutlinedIcon className="icon" />
+          <div className="counter">2</div>
+        </div>
+        <div className="item">
+          <ListOutlinedIcon className="icon" />
+        </div>
+        <div className="item">
+          <img
+            src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="avatar"
+          />
         </div>
       </div>
-      </div>
-    );
-  };
-  
-  export default Navbar;
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
